a5201eb8e06deff13322220ce459c32ce2b0e50b
Rafael Dutra <raffaeldutra@gmail.com>
2019-09-24 22:29:50 -0300

    Nao mais necessario

--- providers/digital-ocean/with-modules/project.tf
+++
@@ -1,9 +0,0 @@
-module "project-web-1" {
-  source  = "raffaeldutra/project/digitalocean"
-  version = "1.0.4"
-
-  name        = terraform.workspace
-  description = "Project description"
-  purpose     = "Project purpose"
-  droplets    = [module.droplet-web-1.urn]
-}
