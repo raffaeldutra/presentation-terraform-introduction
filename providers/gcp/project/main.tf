92c288e3dc830d575c1c48bfa1e0c9719bb453bf
Rafael Dutra <raffaeldutra@gmail.com>
2019-11-11 01:10:49 -0300

    Compute Engine modules

--- providers/gcp/project/main.tf
+++ providers/gcp/project/main.tf
@@ -1,4 +1,40 @@
+variable "project" {
+  type        = string
+  description = "(Required) Used to inform a project. Use the Project ID and not the name itself"
+  default     = "gdg-1-257603"
+}
+
+variable "zones" {
+  type    = list
+  default = ["us-central1-a", "us-central1-b"]
+}
+
 provider "google" {
-  project     = "example-company"
-  region      = "us-central1"
+  // You can pass an another way to use credentials
+  // See here: https://www.terraform.io/docs/providers/google/getting_started.html
+  // By default we're using: export GOOGLE_CLOUD_KEYFILE_JSON=path
+  project = var.project
+  region  = var.gcp["region"]
+}
+
+module "compute_engine" {
+  source = "./modules/ce_instance_template"
+
+  name                 = "gdg"
+  instance_description = "Google Developers Group"
+  project              = var.project
+
+  tags = ["gdg", "gdg-${terraform.workspace}"]
+
+  labels = {
+    environment = terraform.workspace
+    purpose     = "Google Developers Group"
+  }
+}
+
+module "instance_group_manager" {
+  source = "./modules/ce_instance_group_manager"
+
+  zone              = element(var.zones, 1)
+  instance_template = module.compute_engine.self_link
 }
--- providers/gcp/project/projet.tf
+++
@@ -1,4 +0,0 @@
-resource "google_project" "my_project" {
-  name = "Example Company"
-  project_id = "example-company"
-}
